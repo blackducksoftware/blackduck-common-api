fd814dcc0da62f8c2fa8fd11615dfdfde5bcf8c5
Eric Kerwin <ekerwin@blackducksoftware.com>
2018-02-01 15:13:25 -0500

    adding non generated models

---
+++ src/main/java/com/blackducksoftware/integration/hub/model/view/ScanSummaryView.java
@@ -0,0 +1,26 @@
+package com.blackducksoftware.integration.hub.model.view;
+
+import java.lang.reflect.Type;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.blackducksoftware.integration.hub.model.HubView;
+import com.blackducksoftware.integration.hub.model.enumeration.ScanSummaryStatusEnum;
+import com.blackducksoftware.integration.hub.model.generated.view.CodeLocationView;
+
+public class ScanSummaryView extends HubView {
+    public static final Map<String, Type> links = new HashMap<>();
+
+    public static final String CODELOCATION_LINK = "codelocation";
+
+    static {
+        links.put(CODELOCATION_LINK, CodeLocationView.class);
+    }
+
+    public ScanSummaryStatusEnum status;
+    public String statusMessage;
+    public Date createdAt;
+    public Date updatedAt;
+
+}
