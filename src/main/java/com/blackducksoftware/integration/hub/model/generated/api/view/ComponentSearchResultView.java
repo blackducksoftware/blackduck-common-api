dde708cd3569e1ec5ea118b874626c940c9097d3
Eric Kerwin <ekerwin@blackducksoftware.com>
2018-02-02 12:45:55 -0500

    updating to a view

--- src/main/java/com/blackducksoftware/integration/hub/model/generated/api/view/ComponentSearchResultView.java
+++ src/main/java/com/blackducksoftware/integration/hub/model/generated/api/view/ComponentSearchResultView.java
@@ -1,9 +1,9 @@
 package com.blackducksoftware.integration.hub.model.generated.api.view;
 
-import com.blackducksoftware.integration.hub.model.HubModel;
+import com.blackducksoftware.integration.hub.model.HubView;
 
 //this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
-public class ComponentSearchResultView extends HubModel {
+public class ComponentSearchResultView extends HubView {
     public String component;
     public String componentName;
     public String originId;
