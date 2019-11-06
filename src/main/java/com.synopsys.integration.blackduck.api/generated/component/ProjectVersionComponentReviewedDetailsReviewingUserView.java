2f49d4ac193c85e6f95ed2d32d1144d8395907cd
crowleySynopsys <alex.crowley@synopsys.com>
2019-11-06 15:26:26 -0500

    Re-upload for extension errors

--- src/main/java/com.synopsys.integration.blackduck.api/generated/component/ProjectVersionComponentReviewedDetailsReviewingUserView.java
+++ src/main/java/com.synopsys.integration.blackduck.api/generated/component/ProjectVersionComponentReviewedDetailsReviewingUserView.java
@@ -1,52 +1,8 @@
 package com.synopsys.integration.blackduck.api.generated.component;
 
 import com.synopsys.integration.blackduck.api.core.BlackDuckComponent;
-import java.util.Optional;
 
 //this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
-public class ProjectVersionComponentReviewedDetailsReviewingUserView extends BlackDuckComponent {
-	public static final String mediaType = "application/vnd.blackducksoftware.bill-of-materials-6+json";
-
-    private String user;
-    private String firstName;
-    private String username;
-    private String lastName;
-
-    public String getUser() {
-	return user;
-    }
-
-    public void setUser(String user) {
-	this.user = user;
-    }
-
-    public String getFirstName() {
-	return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-	this.firstName = firstName;
-    }
-
-    public String getUsername() {
-	return username;
-    }
-
-    public void setUsername(String username) {
-	this.username = username;
-    }
-
-    public String getLastName() {
-	return lastName;
-    }
-
-    public void setLastName(String lastName) {
-	this.lastName = lastName;
-    }
-
-
-    public String getMediaType() {
-	return mediaType;
-    }
+public class ProjectVersionComponentReviewedDetailsReviewingUserView extends ProjectVersionComponentReviewedDetailsReviewingUserViewV6 {
 
 }
