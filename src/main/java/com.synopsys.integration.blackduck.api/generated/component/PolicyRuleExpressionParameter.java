335315b1a4535e6fb658732509e50d9871026ab3
crowleySynopsys <alex.crowley@synopsys.com>
2019-11-11 15:52:54 -0500

    Deprecated annotations for classes that have new names

--- src/main/java/com.synopsys.integration.blackduck.api/generated/component/PolicyRuleExpressionParameter.java
+++ src/main/java/com.synopsys.integration.blackduck.api/generated/component/PolicyRuleExpressionParameter.java
@@ -20,24 +20,30 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.synopsys.integration.blackduck.api.generated.enumeration;
+package com.synopsys.integration.blackduck.api.generated.component;
 
-import com.synopsys.integration.util.EnumUtils;
+import java.util.List;
+import com.synopsys.integration.blackduck.api.core.BlackDuckComponent;
 
+@Deprecated
+// PolicyRuleExpressionParameter is now called PolicyRuleExpressionExpressionsParametersView
 //this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
-public enum ProjectVersionPolicyStatusComponentVersionPolicyViolationDetailsNameType {
-	NOT_IN_VIOLATION,
-	IN_VIOLATION_OVERRIDDEN,
-	IN_VIOLATION;
+public class PolicyRuleExpressionParameter extends BlackDuckComponent {
+	public static final String mediaType = "application/vnd.blackducksoftware.policy-4+json";
 
-	private String mediaType = "application/vnd.blackducksoftware.bill-of-materials-6+json";
+    private java.util.List<String> values;
 
-	public String getMediaType() {
+    public java.util.List<String> getValues() {
+	return values;
+    }
+
+    public void setValues(java.util.List<String> values) {
+	this.values = values;
+    }
+
+
+    public String getMediaType() {
 	return mediaType;
-	}
+    }
 
-	public String prettyPrint() {
-	return EnumUtils.prettyPrint(this);
-	}
-
-} 
+}
---
+++ src/main/java/com.synopsys.integration.blackduck.api/generated/view/VersionBomPolicyStatusView.java
@@ -0,0 +1,81 @@
+/**
+ * blackduck-common-api
+ *
+ * Copyright (c) 2019 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.blackduck.api.generated.view;
+
+import com.synopsys.integration.blackduck.api.generated.component.ProjectVersionPolicyStatusComponentVersionPolicyViolationDetailsView;
+import java.util.List;
+import com.synopsys.integration.blackduck.api.manual.throwaway.generated.component.NameValuePairView;
+import com.synopsys.integration.blackduck.api.core.BlackDuckComponent;
+import com.synopsys.integration.blackduck.api.generated.enumeration.PolicyStatusType;
+import com.synopsys.integration.blackduck.api.core.BlackDuckView;
+import java.util.Optional;
+
+@Deprecated
+// VersionBomPolicyStatusView is now called ProjectVersionPolicyStatusView
+//this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
+public class VersionBomPolicyStatusView extends BlackDuckView {
+	public static final String mediaType = "application/vnd.blackducksoftware.bill-of-materials-4+json";
+
+    private ProjectVersionPolicyStatusComponentVersionPolicyViolationDetailsView componentVersionPolicyViolationDetails;
+    private String updatedAt;
+    private java.util.List<NameValuePairView> componentVersionStatusCounts;
+    private PolicyStatusType overallStatus;
+
+    public ProjectVersionPolicyStatusComponentVersionPolicyViolationDetailsView getComponentVersionPolicyViolationDetails() {
+	return componentVersionPolicyViolationDetails;
+    }
+
+    public void setComponentVersionPolicyViolationDetails(ProjectVersionPolicyStatusComponentVersionPolicyViolationDetailsView componentVersionPolicyViolationDetails) {
+	this.componentVersionPolicyViolationDetails = componentVersionPolicyViolationDetails;
+    }
+
+    public String getUpdatedAt() {
+	return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+	this.updatedAt = updatedAt;
+    }
+
+    public java.util.List<NameValuePairView> getComponentVersionStatusCounts() {
+	return componentVersionStatusCounts;
+    }
+
+    public void setComponentVersionStatusCounts(java.util.List<NameValuePairView> componentVersionStatusCounts) {
+	this.componentVersionStatusCounts = componentVersionStatusCounts;
+    }
+
+    public PolicyStatusType getOverallStatus() {
+	return overallStatus;
+    }
+
+    public void setOverallStatus(PolicyStatusType overallStatus) {
+	this.overallStatus = overallStatus;
+    }
+
+
+    public String getMediaType() {
+	return mediaType;
+    }
+
+}
