b60397db6233a42574e395d97bb62089e38ec2db
crowleySynopsys <alex.crowley@synopsys.com>
2019-11-18 17:53:58 -0500

    Edited dependency of throwaway.VulnerableComponentView

--- src/main/java/com/synopsys/integration/blackduck/api/manual/throwaway/generated/view/VulnerableComponentView.java
+++ src/main/java/com/synopsys/integration/blackduck/api/manual/throwaway/generated/view/VulnerableComponentView.java
@@ -28,7 +28,7 @@ import java.util.Map;
 import com.synopsys.integration.blackduck.api.core.BlackDuckView;
 import com.synopsys.integration.blackduck.api.core.LinkMultipleResponses;
 import com.synopsys.integration.blackduck.api.core.LinkResponse;
-import com.synopsys.integration.blackduck.api.manual.throwaway.generated.view.VulnerabilityView;
+import com.synopsys.integration.blackduck.api.generated.view.VulnerabilityView;
 import com.synopsys.integration.blackduck.api.manual.throwaway.generated.view.MatchedFileView;
 
 //this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
