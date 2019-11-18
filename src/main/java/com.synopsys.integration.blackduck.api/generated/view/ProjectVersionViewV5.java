2313ffe2a0652e952d7fb26d75a4cfe1cb1edfc1
crowleySynopsys <alex.crowley@synopsys.com>
2019-11-18 17:38:18 -0500

    Updated Version of API

--- src/main/java/com.synopsys.integration.blackduck.api/generated/view/ProjectVersionViewV5.java
+++ src/main/java/com.synopsys.integration.blackduck.api/generated/view/ProjectVersionViewV5.java
@@ -47,33 +47,33 @@ public class ProjectVersionViewV5 extends BlackDuckView {
 	public static final String mediaType = "application/vnd.blackducksoftware.project-detail-5+json";
 
 	public static final Map<String, LinkResponse> links = new HashMap<>();
-        public static final String PROJECT_LINK = "project";
         public static final String CUSTOM_FIELDS_LINK = "custom-fields";
+        public static final String LICENSEREPORTS_LINK = "licenseReports";
         public static final String COMPONENTS_LINK = "components";
-        public static final String CODELOCATIONS_LINK = "codelocations";
         public static final String VULNERABLE_COMPONENTS_LINK = "vulnerable-components";
         public static final String POLICY_STATUS_LINK = "policy-status";
+        public static final String CODELOCATIONS_LINK = "codelocations";
+        public static final String PROJECT_LINK = "project";
         public static final String ISSUES_LINK = "issues";
-        public static final String LICENSEREPORTS_LINK = "licenseReports";
 
-	public static final LinkSingleResponse<ProjectView> PROJECT_LINK_RESPONSE = new LinkSingleResponse<ProjectView>(PROJECT_LINK, ProjectView.class);
 	public static final LinkMultipleResponses<ProjectVersionCustomFieldView> CUSTOM_FIELDS_LINK_RESPONSE = new LinkMultipleResponses<ProjectVersionCustomFieldView>(CUSTOM_FIELDS_LINK, ProjectVersionCustomFieldView.class);
+	public static final LinkMultipleResponses<ReportView> LICENSEREPORTS_LINK_RESPONSE = new LinkMultipleResponses<ReportView>(LICENSEREPORTS_LINK, ReportView.class);
 	public static final LinkMultipleResponses<ProjectVersionComponentView> COMPONENTS_LINK_RESPONSE = new LinkMultipleResponses<ProjectVersionComponentView>(COMPONENTS_LINK, ProjectVersionComponentView.class);
-	public static final LinkMultipleResponses<CodeLocationView> CODELOCATIONS_LINK_RESPONSE = new LinkMultipleResponses<CodeLocationView>(CODELOCATIONS_LINK, CodeLocationView.class);
 	public static final LinkMultipleResponses<VulnerableComponentView> VULNERABLE_COMPONENTS_LINK_RESPONSE = new LinkMultipleResponses<VulnerableComponentView>(VULNERABLE_COMPONENTS_LINK, VulnerableComponentView.class);
 	public static final LinkSingleResponse<ProjectVersionPolicyStatusView> POLICY_STATUS_LINK_RESPONSE = new LinkSingleResponse<ProjectVersionPolicyStatusView>(POLICY_STATUS_LINK, ProjectVersionPolicyStatusView.class);
+	public static final LinkMultipleResponses<CodeLocationView> CODELOCATIONS_LINK_RESPONSE = new LinkMultipleResponses<CodeLocationView>(CODELOCATIONS_LINK, CodeLocationView.class);
+	public static final LinkSingleResponse<ProjectView> PROJECT_LINK_RESPONSE = new LinkSingleResponse<ProjectView>(PROJECT_LINK, ProjectView.class);
 	public static final LinkMultipleResponses<IssueView> ISSUES_LINK_RESPONSE = new LinkMultipleResponses<IssueView>(ISSUES_LINK, IssueView.class);
-	public static final LinkMultipleResponses<ReportView> LICENSEREPORTS_LINK_RESPONSE = new LinkMultipleResponses<ReportView>(LICENSEREPORTS_LINK, ReportView.class);
 
     static {
-	links.put(PROJECT_LINK, PROJECT_LINK_RESPONSE);
 	links.put(CUSTOM_FIELDS_LINK, CUSTOM_FIELDS_LINK_RESPONSE);
+	links.put(LICENSEREPORTS_LINK, LICENSEREPORTS_LINK_RESPONSE);
 	links.put(COMPONENTS_LINK, COMPONENTS_LINK_RESPONSE);
-	links.put(CODELOCATIONS_LINK, CODELOCATIONS_LINK_RESPONSE);
 	links.put(VULNERABLE_COMPONENTS_LINK, VULNERABLE_COMPONENTS_LINK_RESPONSE);
 	links.put(POLICY_STATUS_LINK, POLICY_STATUS_LINK_RESPONSE);
+	links.put(CODELOCATIONS_LINK, CODELOCATIONS_LINK_RESPONSE);
+	links.put(PROJECT_LINK, PROJECT_LINK_RESPONSE);
 	links.put(ISSUES_LINK, ISSUES_LINK_RESPONSE);
-	links.put(LICENSEREPORTS_LINK, LICENSEREPORTS_LINK_RESPONSE);
     }
 
     private ProjectVersionPhaseType phase;
