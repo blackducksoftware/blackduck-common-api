3654335d64b7d3702643903b259270e13fc9af63
Eric Kerwin <ekerwin@synopsys.com>
2021-02-26 15:14:51 -0500

    reducing notification views

--- src/main/java/com/synopsys/integration/blackduck/api/manual/view/NotificationUserView.java
+++ src/main/java/com/synopsys/integration/blackduck/api/manual/view/NotificationUserView.java
@@ -3,29 +3,13 @@
  *
  * Copyright (c) 2021 Synopsys, Inc.
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
 package com.synopsys.integration.blackduck.api.manual.view;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import com.synopsys.integration.blackduck.api.core.BlackDuckResponse;
 import com.synopsys.integration.blackduck.api.core.response.LinkResponse;
 import com.synopsys.integration.blackduck.api.core.response.LinkSingleResponse;
 import com.synopsys.integration.blackduck.api.manual.component.NotificationContentComponent;
@@ -45,32 +29,6 @@ public class NotificationUserView<T extends NotificationContentComponent> extend
     }
 
     private NotificationStateRequestStateType notificationState;
-
-    @Override
-    public Class<? extends BlackDuckResponse> getSubclass() {
-        switch (getType()) {
-            case BOM_EDIT:
-                return BomEditNotificationUserView.class;
-            case LICENSE_LIMIT:
-                return LicenseLimitNotificationUserView.class;
-            case PROJECT:
-                return ProjectNotificationUserView.class;
-            case PROJECT_VERSION:
-                return ProjectVersionNotificationUserView.class;
-            case POLICY_OVERRIDE:
-                return PolicyOverrideNotificationUserView.class;
-            case RULE_VIOLATION_CLEARED:
-                return RuleViolationClearedNotificationUserView.class;
-            case RULE_VIOLATION:
-                return RuleViolationNotificationUserView.class;
-            case VERSION_BOM_CODE_LOCATION_BOM_COMPUTED:
-                return VersionBomCodeLocationBomComputedNotificationUserView.class;
-            case VULNERABILITY:
-                return VulnerabilityNotificationUserView.class;
-            default:
-                return NotificationUserView.class;
-        }
-    }
 
     public NotificationStateRequestStateType getNotificationState() {
         return notificationState;
--- src/main/java/com/synopsys/integration/blackduck/api/manual/view/NotificationView.java
+++ src/main/java/com/synopsys/integration/blackduck/api/manual/view/NotificationView.java
@@ -3,68 +3,21 @@
  *
  * Copyright (c) 2021 Synopsys, Inc.
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
 package com.synopsys.integration.blackduck.api.manual.view;
 
 import java.util.Date;
 
-import com.synopsys.integration.blackduck.api.core.BlackDuckResponse;
 import com.synopsys.integration.blackduck.api.core.BlackDuckView;
 import com.synopsys.integration.blackduck.api.manual.component.NotificationContentComponent;
 import com.synopsys.integration.blackduck.api.manual.enumeration.NotificationType;
 
-public abstract class NotificationView<T extends NotificationContentComponent> extends BlackDuckView {
+public class NotificationView<T extends NotificationContentComponent> extends BlackDuckView {
     private String contentType;
     private java.util.Date createdAt;
     private NotificationType type;
     private T content;
-
-    @Override
-    public boolean hasSubclasses() {
-        return true;
-    }
-
-    @Override
-    public Class<? extends BlackDuckResponse> getSubclass() {
-        switch (type) {
-            case BOM_EDIT:
-                return BomEditNotificationView.class;
-            case LICENSE_LIMIT:
-                return LicenseLimitNotificationView.class;
-            case POLICY_OVERRIDE:
-                return PolicyOverrideNotificationView.class;
-            case PROJECT:
-                return ProjectNotificationView.class;
-            case PROJECT_VERSION:
-                return ProjectVersionNotificationView.class;
-            case RULE_VIOLATION_CLEARED:
-                return RuleViolationClearedNotificationView.class;
-            case RULE_VIOLATION:
-                return RuleViolationNotificationView.class;
-            case VERSION_BOM_CODE_LOCATION_BOM_COMPUTED:
-                return VersionBomCodeLocationBomComputedNotificationView.class;
-            case VULNERABILITY:
-                return VulnerabilityNotificationView.class;
-            default:
-                return NotificationView.class;
-        }
-    }
 
     public T getContent() {
         return content;
