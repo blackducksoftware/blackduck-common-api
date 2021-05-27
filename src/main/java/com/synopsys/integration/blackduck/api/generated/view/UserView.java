720f89f5af556ca79f33f7fb6e38a80a491b72ff
Eric Kerwin <ekerwin@synopsys.com>
2021-05-26 21:42:13 -0400

    adding usergroups

--- src/main/java/com/synopsys/integration/blackduck/api/generated/view/UserView.java
+++ src/main/java/com/synopsys/integration/blackduck/api/generated/view/UserView.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-common-api
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.api.generated.view;
 
 import java.util.HashMap;
@@ -9,6 +16,7 @@ import com.synopsys.integration.blackduck.api.core.response.LinkMultipleResponse
 import com.synopsys.integration.blackduck.api.core.response.UrlMultipleResponses;
 import com.synopsys.integration.blackduck.api.generated.response.AssignedProjectView;
 import com.synopsys.integration.blackduck.api.generated.view.RoleAssignmentView;
+import com.synopsys.integration.blackduck.api.generated.view.UserGroupView;
 import com.synopsys.integration.blackduck.api.manual.view.NotificationUserView;
 
 // this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
@@ -19,17 +27,20 @@ public class UserView extends BlackDuckView {
     public static final String NOTIFICATIONS_LINK = "notifications";
     public static final String PROJECTS_LINK = "projects";
     public static final String ROLES_LINK = "roles";
+    public static final String USERGROUPS_LINK = "usergroups";
 
     public static final LinkMultipleResponses<RoleAssignmentView> INHERITED_ROLES_LINK_RESPONSE = new LinkMultipleResponses<RoleAssignmentView>(INHERITED_ROLES_LINK, RoleAssignmentView.class);
     public static final LinkMultipleResponses<NotificationUserView> NOTIFICATIONS_LINK_RESPONSE = new LinkMultipleResponses<NotificationUserView>(NOTIFICATIONS_LINK, NotificationUserView.class);
     public static final LinkMultipleResponses<AssignedProjectView> PROJECTS_LINK_RESPONSE = new LinkMultipleResponses<AssignedProjectView>(PROJECTS_LINK, AssignedProjectView.class);
     public static final LinkMultipleResponses<RoleAssignmentView> ROLES_LINK_RESPONSE = new LinkMultipleResponses<RoleAssignmentView>(ROLES_LINK, RoleAssignmentView.class);
+    public static final LinkMultipleResponses<UserGroupView> USERGROUPS_LINK_RESPONSE = new LinkMultipleResponses<UserGroupView>(USERGROUPS_LINK, UserGroupView.class);
 
     static {
         links.put(INHERITED_ROLES_LINK, INHERITED_ROLES_LINK_RESPONSE);
         links.put(NOTIFICATIONS_LINK, NOTIFICATIONS_LINK_RESPONSE);
         links.put(PROJECTS_LINK, PROJECTS_LINK_RESPONSE);
         links.put(ROLES_LINK, ROLES_LINK_RESPONSE);
+        links.put(USERGROUPS_LINK, USERGROUPS_LINK_RESPONSE);
     }
 
     private Boolean active;
@@ -126,6 +137,14 @@ public class UserView extends BlackDuckView {
 
     public Optional<UrlMultipleResponses<RoleAssignmentView>> metaRolesLinkSafely() {
         return metaMultipleResponsesSafely(ROLES_LINK_RESPONSE);
+    }
+
+    public UrlMultipleResponses<UserGroupView> metaUsergroupsLink() {
+        return metaMultipleResponses(USERGROUPS_LINK_RESPONSE);
+    }
+
+    public Optional<UrlMultipleResponses<UserGroupView>> metaUsergroupsLinkSafely() {
+        return metaMultipleResponsesSafely(USERGROUPS_LINK_RESPONSE);
     }
 
 }
