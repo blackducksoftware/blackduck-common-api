7a6eb5a56a6b1e843d7a302f9ad4d815d767b906
psantos1113 <psantos@blackducksoftware.com>
2021-10-15 10:18:25 -0400

    notification unknown version (#39)
    * feature: Add ComponentUnknownVersion notification type.
    
    * fix: Use the correct enum for status.
    
    * feat: Add missing critical fields.

---
+++ src/main/java/com/synopsys/integration/blackduck/api/manual/view/ComponentUnknownVersionView.java
@@ -0,0 +1,14 @@
+/*
+ * blackduck-common-api
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
+package com.synopsys.integration.blackduck.api.manual.view;
+
+import com.synopsys.integration.blackduck.api.manual.component.ComponentUnknownVersionContent;
+
+public class ComponentUnknownVersionView extends NotificationView<ComponentUnknownVersionContent> {
+
+}
