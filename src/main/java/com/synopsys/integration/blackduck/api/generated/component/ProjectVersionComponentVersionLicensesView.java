95834804925901751fa105fe4c4c9e7fd833303d
crowleySynopsys <alex.crowley@synopsys.com>
2021-11-08 19:08:31 -0500

    Make ProjectVersionComponentVersionLicensesView.licenses of type List<ProjectVersionComponentVersionLicensesView>.

--- src/main/java/com/synopsys/integration/blackduck/api/generated/component/ProjectVersionComponentVersionLicensesView.java
+++ src/main/java/com/synopsys/integration/blackduck/api/generated/component/ProjectVersionComponentVersionLicensesView.java
@@ -8,6 +8,7 @@
 package com.synopsys.integration.blackduck.api.generated.component;
 
 import com.synopsys.integration.blackduck.api.core.BlackDuckComponent;
+import com.synopsys.integration.blackduck.api.generated.component.ProjectVersionComponentVersionLicensesView;
 import com.synopsys.integration.blackduck.api.generated.enumeration.LicenseOwnershipType;
 import com.synopsys.integration.blackduck.api.generated.enumeration.LicenseType;
 
@@ -16,7 +17,7 @@ public class ProjectVersionComponentVersionLicensesView extends BlackDuckCompone
     private String license;
     private String licenseDisplay;
     private LicenseType licenseType;
-    private java.util.List<String> licenses;
+    private java.util.List<ProjectVersionComponentVersionLicensesView> licenses;
     private LicenseOwnershipType ownership;
     private String spdxId;
 
@@ -44,11 +45,11 @@ public class ProjectVersionComponentVersionLicensesView extends BlackDuckCompone
         this.licenseType = licenseType;
     }
 
-    public java.util.List<String> getLicenses() {
+    public java.util.List<ProjectVersionComponentVersionLicensesView> getLicenses() {
         return licenses;
     }
 
-    public void setLicenses(java.util.List<String> licenses) {
+    public void setLicenses(java.util.List<ProjectVersionComponentVersionLicensesView> licenses) {
         this.licenses = licenses;
     }
 
