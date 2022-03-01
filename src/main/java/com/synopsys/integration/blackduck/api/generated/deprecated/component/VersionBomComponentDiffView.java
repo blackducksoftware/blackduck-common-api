3befaba32334de8148406b31b7bf75c43fc32322
crowleySynopsys <alex.crowley@synopsys.com>
2022-03-01 10:54:07 -0500

    2022.2.0.

--- src/main/java/com/synopsys/integration/blackduck/api/generated/deprecated/component/VersionBomComponentDiffView.java
+++ src/main/java/com/synopsys/integration/blackduck/api/generated/deprecated/component/VersionBomComponentDiffView.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-common-api
+ *
+ * Copyright (c) 2022 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.blackduck.api.generated.deprecated.component;
 
 import java.math.BigDecimal;
