12346f53603a4a7b0a717ee3d5bcd1dfe6dfc0bf
dterrybd <dterry@blackduck.com>
2025-11-17 11:34:43 -0500

    remove archived value

--- src/main/java/com/blackduck/integration/blackduck/api/manual/temporary/enumeration/ProjectVersionPhaseType.java
+++ src/main/java/com/blackduck/integration/blackduck/api/manual/temporary/enumeration/ProjectVersionPhaseType.java
@@ -11,7 +11,6 @@ import com.blackduck.integration.util.EnumUtils;
 
 //this file should not be edited - if changes are necessary, the generator should be updated, then this file should be re-created
 public enum ProjectVersionPhaseType {
-    ARCHIVED,
     DEPRECATED,
     DEVELOPMENT,
     PLANNING,
